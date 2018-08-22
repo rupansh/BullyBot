6542c5d833160e87cb8afb7352e68fb58a48c835
Rupansh Kek <rupanshsekar@hotmail.com>
2018-08-22 07:22:53 +0530

    add /bully functionality

--- bullybot.py
+++ bullybot.py
@@ -4,26 +4,24 @@ TOKEN = "ENTER_TOKEN"
 bot = telebot.TeleBot(TOKEN)
 
 boi = [0]
+bullysubject = []
 
 
 @bot.message_handler(commands=['bullystop'])
 def disable(message):
     boi.append(68)
-    bot.reply_to(message, "stopping bully")
-
-
-@bot.message_handler(commands=['bullystart'])
-def enable(message):
-    boi.append(69)
-    if 68 in boi:
-        boi.remove(68)
-    bot.reply_to(message, "initiate cyber bullying")
+    bot.reply_to(message, "keh")
 
 
 @bot.message_handler()
-def gay(message):
-    if message.from_user.id == int("Telegram_ID") and 69 in boi and 68 not in boi:
-        bot.reply_to(message, "you gay boi")
-
-
-bot.polling()
+def bully(message):
+    if message.text.startswith("/bully "):
+        bullysubject.clear()
+        keh = message.text.replace("/bully ", "", 1)
+        bullysubject.append(int(keh))
+        while 68 in boi:
+            boi.remove(68)
+        bot.reply_to(message, "bullying this nigga")
+    elif message.from_user.id in bullysubject and 68 not in boi:
+        bot.reply_to(message, "Get rekt depressed uncle")
+        print(bullysubject)
