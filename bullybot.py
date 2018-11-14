035ca2d4d314fd90d36c08c6d369b88cd3d0e51b
Rupansh Kek <rupanshsekar@hotmail.com>
2018-11-14 21:50:55 +0530

    bullybot: re-write for aiogram
    async lul

--- bullybot.py
+++ bullybot.py
@@ -1,30 +1,37 @@
-import telebot
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
 TOKEN = "ENTER_TOKEN"
-bot = telebot.TeleBot(TOKEN)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-boi = [0]
+boi = 0
 bullysubject = []
 
 
-@bot.message_handler(commands=['bullystop'])
-def disable(message):
-    boi.append(68)
-    bot.reply_to(message, "keh")
+@dp.message_handler(commands=['bullystop'])
+async def disable(message: types.Message):
+    global boi
+    boi = 68
+    await message.reply("keh")
 
 
-@bot.message_handler()
-def bully(message):
-    if message.text.startswith("/bully "):
-        bullysubject.clear()
-        keh = message.text.replace("/bully ", "", 1)
-        bullysubject.append(int(keh))
-        while 68 in boi:
-            boi.remove(68)
-        bot.reply_to(message, "bullying this boi")
-    elif message.from_user.id in bullysubject and 68 not in boi:
-        bot.reply_to(message, "Get rekt depressed uncle")
+@dp.message_handler(commands=['bully'])
+async def bully(message: types.Message):
+    global boi
+    global bullysubject
+    args = message.get_full_command()
+    bullysubject.append(int(args[1]))
+    boi = 0
+    await message.reply("bullying this boi")
+
+
+@dp.message_handler()
+async def bullyreply(message: types.Message):
+    if message.from_user.id in bullysubject and boi == 0:
+        await message.reply("you gay boi")
         print(bullysubject)
 
-
-bot.polling()
+if __name__ == '__main__':
+    executor.start_polling(dp)
